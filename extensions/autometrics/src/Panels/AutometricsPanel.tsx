8b6f04505ad4a4598de165c69f09fe15e421d5b1
Chao Sui <chao.sui@curvebeamai.com>
2025-08-18 22:01:24 +1000

    wip

--- extensions/autometrics/src/Panels/AutometricsPanel.tsx
+++ extensions/autometrics/src/Panels/AutometricsPanel.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Button } from '@ohif/ui-next';
-import Talas from './Talas';
+import Talas from './View/Talas';
+import Main from './View/Main';
 
 const AutometricsPanel = ({ commandsManager, servicesManager }) => {
   const [currentView, setCurrentView] = useState('autometrics'); // 'autometrics' or 'talas'
@@ -50,73 +51,7 @@ const AutometricsPanel = ({ commandsManager, servicesManager }) => {
 
   // Default Autometrics view
   return (
-    <div className="flex flex-col space-y-6 p-4">
-      <div className="mb-4 text-lg font-semibold text-white">Autometrics</div>
-
-      {/* Angular Measurements Group */}
-      <div className="space-y-3">
-        <div className="border-b border-gray-600 pb-1 text-sm font-medium text-gray-300">
-          Angular Measurements
-        </div>
-        <div className="flex flex-col space-y-2 pl-2">
-          <Button
-            variant="default"
-            className="w-full justify-start text-sm"
-            onClick={() => handleButtonClick('M1M2')}
-          >
-            M1M2
-          </Button>
-
-          <Button
-            variant="default"
-            className="w-full justify-start text-sm"
-            onClick={() => handleButtonClick('TMT-DOR')}
-          >
-            TMT-DOR
-          </Button>
-
-          <Button
-            variant="default"
-            className="w-full justify-start text-sm"
-            onClick={() => handleButtonClick('TMT-LAT')}
-          >
-            TMT-LAT
-          </Button>
-
-          <Button
-            variant="default"
-            className="w-full justify-start text-sm"
-            onClick={() => handleButtonClick('CP')}
-          >
-            CP
-          </Button>
-
-          <Button
-            variant="default"
-            className="w-full justify-start text-sm"
-            onClick={() => handleButtonClick('HA')}
-          >
-            HA
-          </Button>
-        </div>
-      </div>
-
-      {/* Foot Ankle Offset Group */}
-      <div className="space-y-3">
-        <div className="border-b border-gray-600 pb-1 text-sm font-medium text-gray-300">
-          Foot Ankle Offset
-        </div>
-        <div className="flex flex-col space-y-2 pl-2">
-          <Button
-            variant="default"
-            className="w-full justify-start text-sm"
-            onClick={() => handleButtonClick('TALAS')}
-          >
-            TALAS
-          </Button>
-        </div>
-      </div>
-    </div>
+    <Main handleButtonClick={handleButtonClick} />
   );
 };
 
