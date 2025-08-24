c526fb4bb264bf84c50eacbad33f2a70487b2335
Chao Sui <chao.sui@curvebeamai.com>
2025-08-24 14:42:49 +1000

    Add logos

--- extensions/autometrics/src/Panels/Main.tsx
+++ extensions/autometrics/src/Panels/Main.tsx
@@ -80,6 +80,29 @@ const Main = ({ handleButtonClick }) => {
           </Button>
         </div>
       </div>
+
+      {/* CUBEVUE AUTOMETRICS Logo */}
+      <div className="mt-6 flex items-center justify-center border-t border-gray-600 pt-4">
+        <div className="flex w-full flex-col items-center space-y-3">
+          {/* CurveBeam Logo */}
+          <div className="w-full">
+            <img
+              src="/Curvebeam-Logo.png"
+              alt="CurveBeam Logo"
+              className="h-auto w-full object-contain"
+            />
+          </div>
+
+          {/* Autometrics Logo */}
+          <div className="w-full">
+            <img
+              src="/Autometrics.png"
+              alt="Autometrics"
+              className="h-auto w-full object-contain"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- extensions/autometrics/src/Panels/Talas.tsx
+++ extensions/autometrics/src/Panels/Talas.tsx
@@ -881,6 +881,29 @@ function Talas({ setCurrentView, commandsManager }) {
           Submit
         </Button>
       </div>
+
+      {/* CUBEVUE AUTOMETRICS Logo */}
+      <div className="mt-6 flex items-center justify-center border-t border-gray-600 pt-4">
+        <div className="flex w-full flex-col items-center space-y-3">
+          {/* CurveBeam Logo */}
+          <div className="w-full">
+            <img
+              src="/Curvebeam-Logo.png"
+              alt="CurveBeam Logo"
+              className="h-auto w-full object-contain"
+            />
+          </div>
+
+          {/* Autometrics Logo */}
+          <div className="w-full">
+            <img
+              src="/Autometrics.png"
+              alt="Autometrics"
+              className="h-auto w-full object-contain"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
